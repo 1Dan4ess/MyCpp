bcbdfa5383e337d6375c9d2f8ebcfb2fa37cd2f7
zolot_dd00@student.itstep.org <zolot_dd00@student.itstep.org>
2026-05-31 10:15:00 +0300

    .

---
+++ dz5-6/dz5-6.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<Windows.h>
+#include <cstdlib>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(65001);
+	SetConsoleOutputCP(65001);
+
+	//#1
+	//int n, x = 0;
+	//float m;
+	//cin >> m >> n;
+    //int res = 0;
+    //for (size_t b = 0; b <= n; b++) 
+    //{
+    //    for (size_t k = 0; k <= n - b; k++) 
+    //    {
+    //        int t = n - b - k;
+    //        if (b * 10.0 + k * 5.0 + t * 0.5 == m) 
+    //        {
+    //            res = 1;
+    //        }
+    //    }
+    //}
+    //if (res == 1) 
+    //{
+    //    cout << "Yes" << endl;
+    //}
+    //else 
+    //{
+    //    cout << "No" << endl;
+    //}
+
+
+
+
+    //#2
+    //int n, x = 0, b, i = 1;
+    //cin >> n;
+    //do
+    //{
+    //    cin >> b;
+    //    if (b > 100)
+    //    {
+    //        x = i;
+    //    }
+    //    i++;
+    //} while (i <= n);
+    //cout << x << endl;
+}
