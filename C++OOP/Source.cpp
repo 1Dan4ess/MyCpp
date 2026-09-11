06536b67bf1eb4df5ac707c16ad37cbea25f9f33
zolot_dd00@student.itstep.org <zolot_dd00@student.itstep.org>
2026-09-11 19:24:09 +0300

    C++OOP

---
+++ C++OOP/Source.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include"Student.h"
+using namespace std;
+
+int main()
+{
+	Student s1;
+	/*strcpy(s1.name, "John");
+	s1.age = 20;*/
+
+	s1.id = 12345;
+
+	return 0;
+}
---
+++ C++OOP/Student.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class Student
+{
+private:
+	char name[20];
+	int age;
+
+public:
+	int id;
+};
